82f3d38799ef425d1d3ff7709a07d0baea6e2351
jay1261 <dongjae73@naver.com>
2024-01-14 16:21:56 +0900

    change light mode highlighter color post 적용

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705056382';
+const cacheName = 'chirpy-1705216403';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
