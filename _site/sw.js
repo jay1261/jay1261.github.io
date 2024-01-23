f635b0f6b9deec224ecc34c7383d9e2939a6e044
jay1261 <dongjae73@naver.com>
2024-01-23 16:47:05 +0900

    [Add Post] Algorithm - Basic36~39

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705930353';
+const cacheName = 'chirpy-1705995949';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
