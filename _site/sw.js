36a35a66703889f550a8d18431f41d4214b084d7
jay1261 <dongjae73@naver.com>
2024-03-15 15:25:21 +0900

    [add] post - java switch

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1710402259';
+const cacheName = 'chirpy-1710483824';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
