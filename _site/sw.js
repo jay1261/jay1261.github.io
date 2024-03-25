4a714a40dfb57126fc3539c6bd00484320442eb4
jay1261 <dongjae73@naver.com>
2024-03-25 13:48:53 +0900

    폴더 구조 변경

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711338273';
+const cacheName = 'chirpy-1711342081';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
