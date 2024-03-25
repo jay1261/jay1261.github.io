a96d1ff50f2f202cdced136cd1fd595bf67240a3
jay1261 <dongjae73@naver.com>
2024-03-25 12:46:43 +0900

    [add] post - programmers java lv0_1

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711334244';
+const cacheName = 'chirpy-1711338273';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
