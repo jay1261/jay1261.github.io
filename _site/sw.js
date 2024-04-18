4e3874467fb7b886aec142472b1556147accca5c
jay1261 <dongjae73@naver.com>
2024-04-18 11:19:35 +0900

    [style] - sidebar 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1713404812';
+const cacheName = 'chirpy-1713406696';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
