f4f7e354bb483c6ebf2365520e7a070a44c79c2a
jay1261 <dongjae73@naver.com>
2024-04-26 14:58:26 +0900

    [add] post - java string class 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1714092683';
+const cacheName = 'chirpy-1714111085';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
